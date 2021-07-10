a05732a267f0b08a6c4d0d2af89f7185ca0b4ae9
Ngomeli <joshuangomeli@gmail.com>
2021-07-10 16:29:40 +0300

    added user validation

--- app/src/main/java/com/example/myassignment/LoginActivity.kt
+++ app/src/main/java/com/example/myassignment/LoginActivity.kt
@@ -9,8 +9,24 @@ class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
-        btnRegLogin.setOnClickListener{startActivity(Intent(this,RegisterActivity::class.java))
-        overridePendingTransition(R.anim.slide_from_right,R.anim.slide_to_left)
+        var email: String
+        var password: String
+        btnRegLogin.setOnClickListener {
+            startActivity(Intent(this, RegisterActivity::class.java))
+            overridePendingTransition(R.anim.slide_from_right, R.anim.slide_to_left)
+
+
+        }
+        btnLogin.setOnClickListener {
+            email = txtEmail.text.toString()
+            password = txtPassword.text.toString()
+            validateUser(email, password)
+        }
+    }
+
+    fun validateUser(email: String, password: String) {
+        if (email == "joshuangomeli@gmail.com" && password == "ngomeli") {
+            startActivity(Intent(this, MainActivity::class.java))
         }
     }
 }
